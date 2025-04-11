266072c80298d6d97c4cc21779864234b42d1db4
Bekzat <baymukach@gmail.com>
2025-04-11 14:37:55 +0500

    Commit 3

--- config/db2.js
+++ config/db2.js
@@ -1,4 +1,4 @@
 module.exports = {
-    db: 'mongodb://localhost:27017/taskdbb',  // MongoDB қосылу жолы
-    secret: 'some_secret_key'  // Құпия сөз (ол қауіпсіздік үшін қолданылуы мүмкін)
+    db: 'mongodb://localhost:27017/taskdbb', 
+    secret: 'some_secret_key' 
 }
--- server.js
+++ server.js
@@ -10,61 +10,55 @@ mongoose.set('strictQuery', false);
 app.use(cors());
 app.use(express.json());
 
-
-
-mongoose.connect(config.db2, {})
-.then(() => console.log("Baza kosildy"))
-.catch(() => console.error("KAte"))
+mongoose.connect(config.db, {})
+.then(() => console.log("Kosildy"))
+.catch(()=> console.error("KAte"))
 
 const taskSchema = new mongoose.Schema({
-    title: String
+  title: String
 });
+
 const Task = mongoose.model('Task', taskSchema);
 
 
-app.get('/tasks', async (req, res) => {
-  try {
-    const tasks = await Task.find();
-    res.json(tasks);
-  } catch (err) {
-    res.status(500).json({ message: 'Тапсырмаларды алу мүмкін болмады' });
+app.get('/tasks', async(req, res) => {
+  try{
+    const tasks = await  Task.find();
+     res.json(tasks);
+  }catch(err){
+    res.status(500).json({message: "Kate"})
   }
-});
+})
 
-app.post('/tasks', async (req, res) => {
-  try {
-    const newTask = new Task({ 
-      title: req.body.title
-    });
+app.post('/tasks', async(req, res) => {
+  try{
+    const newTask = new Task({title: req.body.title})
     await newTask.save();
-    res.status(201).json(newTask);
-  } catch (err) {
-    res.status(500).json({ message: 'Тапсырма қосу мүмкін болмады' });
+  }catch(err){
+    res.status(500).json({message: "Kate"})
   }
-});
+})
+app.put('/tasks/:id', async(req, res)=> {
+ try{
+  const updateTask = await Task.findByIdAndUpdate(req.params.id, {
+    title: req.body.title
+  }, {
+    new: true
+  })
+  res.json(updateTask);
+ }catch(err){
+  res.status(500).json({message: "Kate"})
+ }
+})
 
-app.put('/tasks/:id', async (req, res) => {
-  try {
-    const updatedTask = await Task.findByIdAndUpdate(req.params.id, 
-      { 
-        title: req.body.title
-      }, 
-      { new: true });
-    res.json(updatedTask);
-  } catch (err) {
-    res.status(500).json({ message: 'Тапсырманы жаңарту мүмкін болмады' });
-  }
-});
-
-app.delete('/tasks/:id', async (req, res) => {
-  try {
+app.delete('/tasks/:id', async(req, res) => {
+  try{
     await Task.findByIdAndDelete(req.params.id);
-    res.json({ message: 'Тапсырма өшірілді' });
-  } catch (err) {
-    res.status(500).json({ message: 'Тапсырманы өшіру мүмкін болмады' });
+    res.json({message: "KOsildy"})
+  }catch(err){
+    res.status(500).json({message: "Kate"})
   }
-});
+})
 
-app.listen(port, () => {
-  console.log(`Сервер http://localhost:${port} жұмыс істеп тұр`);
-});
+
+app.listen(port, () => console.log(`Server: http://localhost:${port}`))
